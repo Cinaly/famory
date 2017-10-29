b17dc7b2eb9581d3ca3cf80d6ef3204c9b7556b3
Cinaly <834518573@qq.com>
2017-10-29 17:56:56 +0800

    init

--- js/header.js
+++ js/header.js
@@ -1,3 +1,20 @@
 /**
  * Created by web-01 on 2017/10/29.
  */
+(()=>{
+    $.ajax({
+        url:"header.html",
+        dataType:"text"
+    }).then(html=>{
+        $("#header").html(html);
+    });
+})();
+
+(()=>{
+    $.ajax({
+        url:"footer.html",
+        dataType:"text"
+    }).then(html=>{
+        $("#footer").html(html);
+    });
+})();
--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,11 @@
 /**
  * Created by web-01 on 2017/10/29.
  */
+(()=>{
+    $.ajax({
+        url:"menu.html",
+        dataType:"text"
+    }).then(html=>{
+        $("#menu").html(html);
+    });
+})();
